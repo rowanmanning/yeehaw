936d17e88939c9039e97ee22e629fd5d6fb6f57b
Rowan Manning <accounts+github@rowanmanning.com>
2020-04-04 15:04:36 +0100

    Start overhauling interactions

--- controller/slack/interact.js
+++ controller/slack/interact.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Const interactions = require('../../lib/interact');
 const SlackWebApiClient = require('@slack/web-api').WebClient;
 
 module.exports = function initSlackInteractController(app) {
@@ -7,59 +8,76 @@ module.exports = function initSlackInteractController(app) {
 	const {Bot, Race} = app.models;
 
 	router.post('/slack/interact', [
+		sendAcknowledgmentResponse,
 		validateRequestBody,
-		getBot,
-		performInteraction,
-		handleError
+		parseJsonPayload,
+		getSlackWebClient,
+		routeInteraction
 	]);
+
+	function sendAcknowledgmentResponse(request, response, next) {
+		response.send('OK');
+		next();
+	}
 
 	function validateRequestBody(request, response, next) {
 		try {
-			response.status(202);
-			response.send('Accepted');
 			// TODO verify message is from Slack
 			if (typeof request.body !== 'object' || Array.isArray(request.body) || request.body === null) {
-				throw new Error('Request body must be an object');
+				throw new Error('Slack interaction was malformed');
 			}
 			if (typeof request.body.payload !== 'string') {
 				throw new Error('Request body must have a payload');
 			}
-			request.body.payload = JSON.parse(request.body.payload);
 			next();
 		} catch (error) {
-			next(error);
+			app.log.error(`Interaction verification failed: ${error.message}`);
 		}
 	}
 
-	async function getBot(request, response, next) {
+	function parseJsonPayload(request, response, next) {
+		request.body.payload = JSON.parse(request.body.payload);
+		next();
+	}
+
+	async function getSlackWebClient(request, response, next) {
 		try {
-			response.locals.bot = await Bot.findOne({
-				slackTeamId: request.body.payload.team.id
+			const bot = await Bot.findOne({
+				slackTeamId: request.body.team_id
 			}).sort({
 				createdAt: -1
 			});
-			if (!response.locals.bot) {
+			if (!bot) {
 				throw new Error('This Slack instance is not authorised');
 			}
+			request.slackWebClient = new SlackWebApiClient(bot.slackBotAccessToken);
 			next();
 		} catch (error) {
-			next(error);
+			app.log.error(`Interaction auth failed: ${error.message}`);
 		}
 	}
 
-	async function performInteraction(request, response, next) {
+	async function routeInteraction(request) {
+		const {slackWebClient} = request;
 		try {
-			for (const action of request.body.payload.actions) {
-				if (action.action_id === 'bet') {
-					await performBetAction(action, request.body.payload, response.locals.bot);
+			// Const interactionType = request.body.payload.type;
+			app.log.info('PAYLOAD', request.body.payload);
+
+			// Temporary to keep bets working for now
+			if (Array.isArray(request.body.payload.actions)) {
+				for (const action of request.body.payload.actions) {
+					if (action.action_id === 'bet') {
+						await performBetAction(action, request.body.payload, slackWebClient);
+					}
 				}
 			}
+
 		} catch (error) {
-			next(error);
+			app.log.error(`Interaction failed: ${error.message}`);
 		}
 	}
 
-	async function performBetAction(action, payload, bot) {
+	async function performBetAction(action, payload, slackWebClient) {
 		const race = await Race.findOne({
 			horses: {
 				$elemMatch: {
@@ -72,16 +90,12 @@ module.exports = function initSlackInteractController(app) {
 		}
 
 		await race.placeBet({
-			slackWebClient: new SlackWebApiClient(bot.slackBotAccessToken),
+			slackWebClient,
 			horseId: action.value,
 			raceId: race._id,
 			slackChannelId: payload.channel.id,
 			slackUserId: payload.user.id
 		});
-	}
-
-	function handleError(error, request, response, next) { // eslint-disable-line no-unused-vars
-		app.log.error(`Interaction error: ${error.message}`);
 	}
 
 };
