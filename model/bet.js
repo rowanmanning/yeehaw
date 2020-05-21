3dc89623c5cdf22374dfadf64f44a4b7ef24fe03
Rowan Manning <accounts+github@rowanmanning.com>
2020-05-21 14:04:38 +0100

    Add the ability to get betting ratios

--- model/bet.js
+++ model/bet.js
@@ -40,5 +40,31 @@ module.exports = function initBetModel(app) {
 		return this.find({horseId: {$in: winningHorseIds}});
 	});
 
+	betSchema.static('getBettingRatiosForTeam', async teamId => {
+		const races = await app.models.Race.find({teamId});
+		const winningHorseIds = races
+			.reduce((horses, race) => {
+				return horses.concat(race.horses.filter(horse => horse.finishingPosition === 1));
+			}, [])
+			.map(horse => horse.id);
+		const bets = await app.models.Bet.find({slackTeamId: teamId});
+		const betRatiosByUser = bets.reduce((ratios, bet) => {
+			ratios[bet.slackUserId] = ratios[bet.slackUserId] || {
+				userId: bet.slackUserId,
+				bets: 0,
+				wins: 0
+			};
+			ratios[bet.slackUserId].bets += 1;
+			if (winningHorseIds.includes(bet.horseId)) {
+				ratios[bet.slackUserId].wins += 1;
+			}
+			return ratios;
+		}, {});
+		return Object.values(betRatiosByUser).map(userBettingDetails => {
+			userBettingDetails.ratio = userBettingDetails.wins / userBettingDetails.bets;
+			return userBettingDetails;
+		});
+	});
+
 	return betSchema;
 };
